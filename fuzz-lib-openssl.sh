10a8db69c204def4b2f58174593e5888fb3788f3
Toralf Förster <toralf.foerster@gmx.de>
2021-05-14 18:03:29 +0200

    variable name spelling

--- fuzz-lib-openssl.sh
+++ fuzz-lib-openssl.sh
@@ -21,13 +21,13 @@ function repoWasUpdated() {
 function buildFuzzers() {
   cd ~/$software
 
-  local config_options="enable-fuzz-afl no-shared no-module
+  local options="enable-fuzz-afl no-shared no-module
     -DPEDANTIC enable-tls1_3 enable-weak-ssl-ciphers enable-rc5
     enable-md2 enable-ssl3 enable-ssl3-method enable-nextprotoneg
     enable-ec_nistp_64_gcc_128 -fno-sanitize=alignment
     --debug"
 
-  if ! ./config $config_options; then
+  if ! ./config $options; then
     return 3
   fi
 

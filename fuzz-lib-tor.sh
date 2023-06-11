45fe263166d3524116dfcddf67d0497a6355948b
Toralf Förster <toralf.foerster@gmx.de>
2023-06-11 13:22:53 +0200

    adapt recent configure options

--- fuzz-lib-tor.sh
+++ fuzz-lib-tor.sh
@@ -18,15 +18,10 @@ function configureSoftware() {
 
   if [[ ! -f Makefile ]]; then
     # use the configure options from the official Gentoo ebuild
-    # but: disable coverage (huge slowdown) + enable zstd-advanced-apis
     local gentoo="
-        --prefix=/usr --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --mandir=/usr/share/man --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --disable-dependency-tracking --disable-silent-rules --docdir=/usr/share/doc/tor-9999 --htmldir=/usr/share/doc/tor-9999/html --libdir=/usr/lib64 --localstatedir=/var --enable-system-torrc --disable-android --disable-html-manual --disable-libfuzzer --enable-missing-doc-warnings --disable-module-dirauth --enable-pic --disable-rust --disable-restart-debugging --disable-zstd-advanced-apis --enable-asciidoc --enable-manpage --enable-lzma --enable-libscrypt --enable-seccomp --enable-module-relay --disable-systemd --enable-gcc-hardening --enable-linker-hardening --disable-unittests --disable-coverage --enable-zstd
+      --prefix=/usr --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --mandir=/usr/share/man --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --datarootdir=/usr/share --disable-dependency-tracking --disable-silent-rules --docdir=/usr/share/doc/tor-9999 --htmldir=/usr/share/doc/tor-9999/html --libdir=/usr/lib64 --localstatedir=/var --disable-all-bugs-are-fatal --enable-system-torrc --disable-android --disable-coverage --disable-html-manual --disable-libfuzzer --enable-missing-doc-warnings --disable-module-dirauth --enable-pic --disable-restart-debugging --disable-zstd-advanced-apis --enable-asciidoc --enable-manpage --enable-lzma --enable-libscrypt --enable-seccomp --enable-module-relay --disable-systemd --enable-gcc-hardening --enable-linker-hardening --enable-unittests --enable-zstd
     "
-    local override="
-        --enable-module-dirauth --enable-zstd-advanced-apis --enable-unittests --disable-coverage
-    "
-
-    ./configure $gentoo $override
+    ./configure $gentoo --enable-module-dirauth
   fi
 }
 

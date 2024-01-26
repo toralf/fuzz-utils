33fa69b020c9dd67e6947869ff3ea1303dc8c7e6
Toralf Förster <toralf.foerster@gmx.de>
2024-01-26 22:25:39 +0100

    add cgroup cpu capping

--- fuzz-cgroup.sh
+++ fuzz-cgroup.sh
@@ -6,17 +6,23 @@ function CreateCgroup() {
   local name=$cgdomain/${1?}
   local pid=${2?}
 
+  # put all fuzzers under 1 sub group
   if [[ ! -d $cgdomain ]]; then
     mkdir $cgdomain
-    echo "+cpu +memory" >$cgdomain/cgroup.subtree_control
+    echo "+cpu +cpuset +memory" >$cgdomain/cgroup.subtree_control
 
-    echo "400" >$cgdomain/cpu.weight
+    # 4 vCPU for all
+    echo "$((4 * 100))" >$cgdomain/cpu.weight
+    echo "$((4 * 100000)) 100000" >$cgdomain/cpu.max
     echo "40G" >$cgdomain/memory.max
     echo "20G" >$cgdomain/memory.swap.max
   fi
 
   mkdir $name || return 13
   echo "$pid" >$name/cgroup.procs
+  # 1 vCPU per fuzzer
+  echo "$((100 * 1))" >$name/cpu.weight
+  echo "$((100000 * 1))" >$name/cpu.max
   echo "8G" >$name/memory.max
   echo "0G" >$name/memory.swap.max
 }

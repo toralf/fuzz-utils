fcd45379b51c05bf0862ab9cf5eeed74241c862a
Toralf Förster <toralf.foerster@gmx.de>
2025-03-29 19:56:02 +0100

    build Tor from scratch

--- fuzz-lib-tor.sh
+++ fuzz-lib-tor.sh
@@ -1,22 +1,19 @@
 # shellcheck shell=bash
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 # specific routines to fuzz Tor
 
 function buildSoftware() {
+  rm -f configure Makefile
+  ./autogen.sh
+
+  # use the configure options from the official Gentoo ebuild
+  local gentoo="
+    --prefix=/usr --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --mandir=/usr/share/man --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --datarootdir=/usr/share --disable-dependency-tracking --disable-silent-rules --docdir=/usr/share/doc/tor-9999 --htmldir=/usr/share/doc/tor-9999/html --libdir=/usr/lib64 --localstatedir=/var --disable-all-bugs-are-fatal --enable-system-torrc --disable-android --disable-coverage --disable-html-manual --disable-libfuzzer --enable-missing-doc-warnings --disable-module-dirauth --enable-pic --disable-restart-debugging --enable-gpl --enable-module-pow --enable-asciidoc --enable-manpage --enable-lzma --enable-libscrypt --enable-seccomp --enable-module-relay --disable-systemd --enable-gcc-hardening --enable-linker-hardening --disable-unittests --enable-zstd
+  "
+  ./configure $gentoo --enable-module-dirauth
   make clean
 
-  if [[ ! -x ./configure ]]; then
-    rm -f Makefile
-    ./autogen.sh
-  fi
-
-  if [[ ! -f Makefile ]]; then
-    # use the configure options from the official Gentoo ebuild
-    local gentoo="
-      --prefix=/usr --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --mandir=/usr/share/man --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --datarootdir=/usr/share --disable-dependency-tracking --disable-silent-rules --docdir=/usr/share/doc/tor-9999 --htmldir=/usr/share/doc/tor-9999/html --libdir=/usr/lib64 --localstatedir=/var --disable-all-bugs-are-fatal --enable-system-torrc --disable-android --disable-coverage --disable-html-manual --disable-libfuzzer --enable-missing-doc-warnings --disable-module-dirauth --enable-pic --disable-restart-debugging --enable-gpl --enable-module-pow --enable-asciidoc --enable-manpage --enable-lzma --enable-libscrypt --enable-seccomp --enable-module-relay --disable-systemd --enable-gcc-hardening --enable-linker-hardening --disable-unittests --enable-zstd
-    "
-    ./configure $gentoo --enable-module-dirauth
-  fi
   make micro-revision.i # https://gitlab.torproject.org/tpo/core/tor/-/issues/29520
   nice -n 3 make -j 1 fuzzers
 }

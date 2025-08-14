9ccaef6c08473edd886666987b27767c718bbe88
Toralf Förster <toralf.foerster@gmx.de>
2025-08-14 10:25:01 +0200

    fix perms for /run

--- bwrap.sh
+++ bwrap.sh
@@ -5,7 +5,6 @@
 # user.max_user_namespaces must be greater than zero
 # example call:
 # bwrap.sh simple-http-server.py --address 1.2.3.4 --port 56789 --directory /tmp/www
-# https://github.com/containers/bubblewrap
 
 set -euf
 export LANG=C.utf8
@@ -17,15 +16,15 @@ sandbox=(env -i
   --level-prefix
   --unshare-all
   --share-net
-  --new-session
   --ro-bind / /
   --dev /dev
   --dev-bind /dev/console /dev/console
   --mqueue /dev/mqueue
   --perms 1777 --tmpfs /dev/shm
   --proc /proc
-  --tmpfs /run
+  --perms 0755 --tmpfs /run
   --ro-bind /sys /sys
+  --new-session
 )
 
 exec "${sandbox[@]}" -- "${@}"
